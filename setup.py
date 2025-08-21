d311a6f00600b48f1db54126e825d9bf6063679f
offerrall <offerrallps4@gmail.com>
2025-08-21 11:43:02 +0200

    aa

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="pygrbl_streamer",
       version="1.0.0",
       author="Beltrán Offerrall",
-      py_modules=["grbl_streamer"],
+      packages=['pygrbl_streamer'],
       install_requires=["pyserial"],
       python_requires=">=3.7"
       )
